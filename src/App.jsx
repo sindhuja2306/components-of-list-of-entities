23a342f181e154abcdb25416a7c8e6e7ae3f6cb8
paila sindhuja <sindhujapaila102@gmail.com>
2025-03-29 15:11:41 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React from 'react';
+import BookCard from './components/bookcard';
 
 function App() {
   const books = [
@@ -34,7 +35,16 @@ function App() {
     },
   ];
 
-  return <>{/* write code to display all the books using */}</>;
+  return(
+    <div>
+      <h1>Digital Library</h1>
+      <div>
+        {books.map((book)=>{
+          <BookCard key={book.id} book={book}/>
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,15 @@
 // write the book component code here
+import React from "react";
+
+const BookCard = ({book})=>{
+    return(
+        <div>
+            <img src={book.image}/>
+            <h2>{book.name}</h2>
+            <p>Genere:{book.genere}</p>
+            <p>Author: {book.author}</p>
+        </div>
+    );
+};
+
+export default BookCard;
